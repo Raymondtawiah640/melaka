04896f3cf31e163c21ecd6c6730e52a029280faa
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-08-13 21:28:13 -0900

    Modified

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,21 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Loading } from '../loading/loading';
 
 @Component({
   selector: 'app-about',
-  imports: [Loading, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './about.html',
   styleUrls: ['./about.css']
 })
-export class About implements OnInit {
-  loading = true;
-
-  ngOnInit() {
-    // Hide loader after 3 seconds
-    setTimeout(() => {
-      this.loading = false;
-    }, 3000);
-  }
-
+export class About{
 }
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true, // ✅ Required for standalone component
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class Navbar {}
+export class Navbar {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  screenIsSmall(): boolean {
+    return window.innerWidth < 768;
+  }
+}
