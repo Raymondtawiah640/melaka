3fbcd797a23e95aa0a29505637ce6233accfc61f
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-08-20 09:15:02 -0900

    modified

--- src/app/software-development/software-development.ts
+++ src/app/software-development/software-development.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-software-development',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './software-development.html',
   styleUrl: './software-development.css'
 })
