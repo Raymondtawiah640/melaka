6768e88f4e4fa9a3eb46f400e83fbf8c9a8a2314
Raymondtawiah640 <enterprisekiln@gmail.com>
2025-09-05 16:17:32 -0900

    modified

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  standalone: true, // makes it standalone if you're using Angular 15+
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './gallery.html',
-  styleUrls: ['./gallery.css'] // <-- should be styleUrls (plural)
+  styleUrls: ['./gallery.css']
 })
 export class Gallery {
   selectedImage: string | null = null;
+
+  images = [
+    'assets/shirt1.jpg',
+    'assets/Cup.jpg',
+    'assets/New.jpg',
+    'assets/shirt.jpg',
+    'assets/g2.jpg',
+    'assets/g1.jpg',
+    'assets/g3.jpg',
+    'assets/g3.jpg'
+  ];
 
   openImage(image: string) {
     this.selectedImage = image;
